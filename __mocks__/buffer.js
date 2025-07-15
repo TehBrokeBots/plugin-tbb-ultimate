3e1f88e4a2d365044cffea4b4adc3bbdcd62ac8e
TehBrokeBots <brobotian@gmail.com>
2025-07-15 07:01:50 -0400

    Delete __mocks__ directory

--- __mocks__/buffer.js
+++
@@ -1,9 +0,0 @@
-// Mock buffer module for Jest compatibility with Bun
-module.exports = {
-  Buffer: {
-    from: (data) => data,
-    alloc: (size) => new Uint8Array(size),
-    allocUnsafe: (size) => new Uint8Array(size),
-    isBuffer: (obj) => obj instanceof Uint8Array,
-  }
-}; 
